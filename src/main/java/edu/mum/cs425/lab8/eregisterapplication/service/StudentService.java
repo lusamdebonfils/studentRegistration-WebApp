bbcb6f24d1d39ac2f71ecf3e6934cefee73c0461
lusamdebonfils <samuelluswata@gmail.com>
2019-08-04 18:29:33 -0500

    added student service functionality and implementation

--- src/main/java/edu/mum/cs425/lab8/eregisterapplication/service/StudentService.java
+++ src/main/java/edu/mum/cs425/lab8/eregisterapplication/service/StudentService.java
@@ -1,5 +1,13 @@
 package edu.mum.cs425.lab8.eregisterapplication.service;
 
-public interface StudentService {
+import edu.mum.cs425.lab8.eregisterapplication.model.Student;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService {
+    public List<Student> getAllStudents();
+    public Student saveBook(Student student);
+    public Student getStudentById(Long studentID);
+    public void deleteStudentById(Long studentID);
 }
--- src/main/java/edu/mum/cs425/lab8/eregisterapplication/service/implementation/StudentServiceImplementation.java
+++ src/main/java/edu/mum/cs425/lab8/eregisterapplication/service/implementation/StudentServiceImplementation.java
@@ -1,6 +1,38 @@
 package edu.mum.cs425.lab8.eregisterapplication.service.implementation;
 
+import edu.mum.cs425.lab8.eregisterapplication.model.Student;
+import edu.mum.cs425.lab8.eregisterapplication.repository.StudentRepository;
 import edu.mum.cs425.lab8.eregisterapplication.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 public class StudentServiceImplementation implements StudentService {
+
+    @Autowired
+    public StudentRepository studentRepo;
+
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepo.findAll((Sort) Comparator.comparing(Student::getFirstName));
+    }
+
+    @Override
+    public Student saveBook(Student student) {
+        return studentRepo.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long studentID) {
+        return studentRepo.findById(studentID).orElse(null);
+    }
+
+    @Override
+    public void deleteStudentById(Long studentID) {
+        studentRepo.deleteById(studentID);
+    }
 }
