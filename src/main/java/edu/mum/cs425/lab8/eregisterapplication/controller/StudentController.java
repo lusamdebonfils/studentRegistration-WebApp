9c1be7179263d961d0d61a7692c7e187e7a3e8e3
lusamdebonfils <samuelluswata@gmail.com>
2019-08-05 07:43:28 -0500

    Added routing to the different links, also worked on  the navbar and put some padding and margins

--- src/main/java/edu/mum/cs425/lab8/eregisterapplication/controller/StudentController.java
+++ src/main/java/edu/mum/cs425/lab8/eregisterapplication/controller/StudentController.java
@@ -4,6 +4,7 @@ import edu.mum.cs425.lab8.eregisterapplication.model.Student;
 import edu.mum.cs425.lab8.eregisterapplication.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +21,13 @@ public class StudentController {
         modelAndView.setViewName("student/list");
         return modelAndView;
     }
+
+    @GetMapping(value = {"/eregister/student/new"})
+    public String displayNewBookForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "student/add";
+    }
+
 
 
 }
